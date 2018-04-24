5ba03de064dc39b993a99527b1b32841094fe41b
Jherico-Ryan Tapang <jhericot@outlook.com>
2018-04-24 10:57:22 -0400

    added to do

--- src/ATMTransaction.java
+++ src/ATMTransaction.java
@@ -1,4 +1,4 @@
-public abstract class ATMTransaction {
+public abstract class ATMTransaction implements Comparable<ATMTransaction> {
 
     String IDcode;
     float amount;
@@ -7,4 +7,9 @@ public abstract class ATMTransaction {
     // TODO:  see Project dot 3...
     // TODO:  TreeSet<E> requires class E to implement Comparable<E> or Comparator<E>
     // TODO: by providing, respectively, compareTo or compare function to define a linear ordering.
+
+
+    /* TODO: implement compareTo method here
+
+     */
 }
